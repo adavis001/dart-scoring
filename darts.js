b2ae378bf1125d44558e2d9bdb28953151845a4f
Alexander Davis <akonidavis@gmail.com>
2017-04-01 17:03:18 -1000

    darts FTW

--- darts.js
+++ darts.js
@@ -6,18 +6,15 @@ scoreThrows = function(radius){
 	var score = 0;
 	for(i = 0; i<radius.length; i++){
 		if(radius[i] > 10){
-			//console.log(radius[i]);
 			score += 0;
 		}
 		else if (radius[i] >= 5) {
-			//console.log(radius[i]);
 			score += 5;
 		}
 		else if(radius[0] <5 && radius[1] <5 && radius[2] <5){
 			score +=43.333;
 		}
 		else if (radius[i] < 5){
-			//console.log(radius[i]);
 			score += 10;
 		}
 	}
